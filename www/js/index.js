4b8706ef5aff79e8a093917202180c8592a3102f
olvrdu44 <olivier@rainetcom.fr>
2018-05-29 18:09:09 +0200

    v291809

--- www/js/index.js
+++ www/js/index.js
@@ -585,6 +585,19 @@ var app = {
 									console.log("Push Subscription state changed: " + JSON.stringify(state));
 								});
 								
+								var notificationOpenedCallback = function(jsonData) {
+									console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+								};
+								
+								window.plugins.OneSignal.startInit("424818bf-2ba9-490a-99a3-d31ccbc93993")
+								.handleNotificationOpened(notificationOpenedCallback)
+								.getPermissionSubscriptionState(function(status) {
+
+								  alert("statut:" + status.subscriptionStatus.userId); // String: OneSignal Player ID
+
+								})
+								.endInit();
+								
 								
 								/*********************************************************/
 							}
@@ -623,17 +636,7 @@ var app = {
 				getImage();
 			});
 			/*********************************************************/
-			var notificationOpenedCallback = function(jsonData) {
-				console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-			};
-			window.plugins.OneSignal.startInit("424818bf-2ba9-490a-99a3-d31ccbc93993")
-			.handleNotificationOpened(notificationOpenedCallback)
-			.getPermissionSubscriptionState(function(status) {
-
-			  alert("statut:" + status.subscriptionStatus.userId); // String: OneSignal Player ID
-
-			})
-			.endInit();
+			
 		});
 		/********************************************************************/
     },
