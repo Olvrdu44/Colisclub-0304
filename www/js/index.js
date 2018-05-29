b179de3442f56c04e87e7a3ca8c0cd6bd969e197
olvrdu44 <olivier@rainetcom.fr>
2018-05-29 17:43:41 +0200

    v291743

--- www/js/index.js
+++ www/js/index.js
@@ -623,16 +623,7 @@ var app = {
 				getImage();
 			});
 			/*********************************************************/
-			var notificationOpenedCallback = function(jsonData) {
-				console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-			};
-			window.plugins.OneSignal.startInit("424818bf-2ba9-490a-99a3-d31ccbc93993").handleNotificationOpened(notificationOpenedCallback).endInit();
 			
-			window.plugins.OneSignal.getPermissionSubscriptionState(function(status) {
-
-			  alert("statut:" + status.subscriptionStatus.userId); // String: OneSignal Player ID
-
-			});
 		});
 		/********************************************************************/
     },
@@ -659,6 +650,17 @@ var app = {
 };
 
 app.initialize();
+
+var notificationOpenedCallback = function(jsonData) {
+	console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+};
+window.plugins.OneSignal.startInit("424818bf-2ba9-490a-99a3-d31ccbc93993").handleNotificationOpened(notificationOpenedCallback).endInit();
+
+window.plugins.OneSignal.getPermissionSubscriptionState(function(status) {
+
+  alert("statut:" + status.subscriptionStatus.userId); // String: OneSignal Player ID
+
+});
 
 /***************************** FONCTION UPLOAD PHOTO ***********************/
 function getImage() 
