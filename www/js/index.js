18bf42f45f859b7db702b6c8294ea0ab8bf87c93
olvrdu44 <olivier@rainetcom.fr>
2018-05-29 18:27:31 +0200

    v291827

--- www/js/index.js
+++ www/js/index.js
@@ -478,19 +478,8 @@ var app = {
 					$("html, body").animate({scrollTop: 0},"slow");
 				}
 			});
-			var notificationOpenedCallback = function(jsonData) {
-									console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
-								};
 								
-								window.plugins.OneSignal
-								.startInit("424818bf-2ba9-490a-99a3-d31ccbc93993")
-								.handleNotificationOpened(notificationOpenedCallback)
-								.endInit();
 								
-								window.plugins.OneSignal.getPermissionSubscriptionState(function(status) 
-								{
-									alert("statut:" + status.subscriptionStatus.userId); // String: OneSignal Player ID
-								});
 			/********************AJAX CONNEXION ***********************/
 			$("#login form").submit(function(e)
 			{
