f8b90e7b413d4848069cf0aba867d7486ecff64b
olvrdu44 <olivier@rainetcom.fr>
2018-05-29 19:37:21 +0200

    v291937

--- www/js/index.js
+++ www/js/index.js
@@ -22,7 +22,7 @@ var app = {
     initialize: function() 
 	{
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-		cordova.plugins.backgroundMode.enable();
+		//cordova.plugins.backgroundMode.enable();
 
 		$(".hophop").click(function()
 		{
